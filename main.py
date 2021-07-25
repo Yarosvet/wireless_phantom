aa3e4b931be512c03f0eca957f85ca7fb8760c64
Yarosvet <000xsz839>
2021-07-25 17:48:54 +0300

    AP starting

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import subprocess
 import os
 
 
-os.system("hostapd hostapd.conf")
+def main():
+    configfile = os.path.join(os.path.abspath(__file__)[:-len(os.path.split(os.path.abspath(__file__))[-1])],
+                              "hostapd.conf")
+    subprocess.call(args=["hostapd", "-B", configfile])
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as exc:
+        subprocess.call(args=["killall", "hostapd"])
+        raise exc
