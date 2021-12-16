f58fa4498d06ed4ad3559c1fef90567936cd3947
Yarosvet <yarosvetk@gmail.com>
2021-12-16 18:06:38 +0300

    Flask server in private LAN

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from pyaccesspoint import AccessPoint
 import logging
-import time
+from flask import Flask, render_template
 
-logging.basicConfig(level=logging.DEBUG)
-ap = AccessPoint(wlan="wlan1", ip="192.168.50.1", ssid="WirelessPhantom", password="besecure6", inet="eth0")
-ap.start()
-time.sleep(50)
-ap.stop()
+HOST = "192.168.50.1"
+app = Flask(__name__)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    ap = AccessPoint(wlan="wlan1", ip=HOST, ssid="WirelessPhantom", password="besecure6", inet="eth0")
+    ap.start()
+    app.run(HOST, 8080)
+    ap.stop()
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    main()
